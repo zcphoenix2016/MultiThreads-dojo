d42c2412df2d51b0ebd2c5d8e8eb5188d8482778
zcphoenix2016 <13564240769@139.com>
2016-12-27 11:42:01 +0800

    TDD framework

---
+++ Source/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+void hello()
+{
+    std::cout << "Hello Concurrent World." << std::endl;
+}
+
+int main()
+{
+    std::thread t(hello);
+
+    t.join();
+
+    return 0;
+}
